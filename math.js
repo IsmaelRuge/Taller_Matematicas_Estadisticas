532718c7d8235b9f6b28b50a60c242843e956503
Ismael Ruge <ismaelruge@gmail.com>
2023-11-16 22:34:04 -0500

    Altura de Triángulo Isosceles y Escaleno.

--- math.js
+++ math.js
@@ -9,11 +9,13 @@ do {
                             4. Área de un Triángulo.
                             5. Circunferencia de un Circulo.
                             6. Área de un Circulo.
+                            7. Altura de un Triángulo Isosceles.
+                            8. Altura de un Triángulo Escaleno.
                             0. Salir.`);
 
         calcular = parseInt(calcular);
 
-        if (calcular >= 0 && calcular <= 6) {
+        if (calcular >= 0 && calcular <= 8) {
             break; 
         } else {
             alert('El código digitado es inválido, por favor verifique e intente nuevamente.');
@@ -38,6 +40,12 @@ do {
             break;
         case 6:
             AreaCirculo();
+            break;
+        case 7:
+            AlturaTrianguloIsosceles();
+            break;
+        case 8:
+            AlturaTrianguloEscaleno();
             break;
         case 0:
             alert('Ha salido.');
@@ -97,4 +105,42 @@ function AreaCirculo(){
     radio = parseInt(radio);
     const areaCirculo = Math.pow(radio, 2) * Math.PI;
     alert('El área del circulo es: ' + areaCirculo.toFixed(2));
+}
+
+function AlturaTrianguloIsosceles(){
+    let l = prompt(`Ingrese el valor del lado en cm.`);
+    l = parseInt(l);
+
+    let b = prompt(`Ingrese el valor de la base en cm.`);
+    b = parseInt(b);
+
+    if(l1 == b){
+        alert('Este no es un triángulo isosceles.');
+        return;
+    } 
+
+    const alt = Math.sqrt(((l**2) - (b**2)) / 4);
+    alert(`La altura del tiángulo isosceles es de: ` + alt);
+}
+
+function AlturaTrianguloEscaleno(){
+    let l1 = prompt(`Ingrese el valor del lado 1 en cm.`);
+    l1 = parseInt(l1);
+    let l2 = prompt(`Ingrese el valor del lado 2 en cm.`);
+    l2 = parseInt(l2);
+    let l3 = prompt(`Ingrese el valor del lado 3 en cm.`);
+    l3 = parseInt(l3);
+
+    if(l1 == l2 || l1 == l3 || l2 == l3){
+        alert('los valores digitados no pueden ser iguales ya que es un triángulo escaleno y todos sus lados son diferentes.');
+        return;
+    }
+
+    let s = (l1 + l2 + l3) / 2;
+
+    let area = Math.sqrt(s * (s - l1) * (s - l2) * (s - l3));
+
+    let altura = Math.floor(2 * area / l1);
+
+    alert('La altura del triángulo escaleno es: ' + altura);
 }
